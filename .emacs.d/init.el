7c9b74b484d949c80f44a80f47cd5b1b98da14ad
Takafumi ARAKAKI <takafumi@takafumi-bdc2.(none)>
2009-01-14 02:15:13 +0900

    句読点

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,6 +34,9 @@
 ;======================================================================
 (load-library "anthy")
 (setq default-input-method "japanese-anthy")
+; 句読点
+(anthy-change-hiragana-map "," "，")
+(anthy-change-hiragana-map "." "．")
 ;;
 ;=======================================================================
 ;フォント
