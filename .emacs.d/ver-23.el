0a3f7ea03da51171b3ae099e4bb7ab4eeec920a5
Takafumi ARAKAKI <takafumi@takafumi-bdc2.(none)>
2009-01-19 04:35:44 +0900

    commit from Makefile

--- .emacs.d/ver-23.el
+++ .emacs.d/ver-23.el
@@ -1,6 +1,13 @@
 (set-default-font "Bitstream Vera Sans Mono-10")
+;tooltipとtabbarのフォント
+(set-face-font 'variable-pitch "Bitstream Vera Sans Mono-9")
+;複数フレームに対応
+(modify-all-frames-parameters (list (assq 'font (frame-parameters))))
+;日本語フォント
 (set-fontset-font (frame-parameter nil 'font)
                   'japanese-jisx0208
                   '("VL ゴシック" . "unicode-bmp"))
+;変換のタイムラグなしに
 (setq anthy-accept-timeout 1)
+;YaTeX使うため
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/yatex/")
