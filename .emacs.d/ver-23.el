a7b0cd6a90c5abddbab4302c9b0492895bcd1cad
Takafumi ARAKAKI <takafumi@takafumi-bdc2.(none)>
2009-04-07 20:58:00 +0900

    ver-23 font

--- .emacs.d/ver-23.el
+++ .emacs.d/ver-23.el
@@ -1,16 +1,13 @@
 (set-default-font "VL Gothic")
-;(set-default-font "Bitstream Vera Sans Mono-10")
-;tooltipとtabbarのフォント
-;(set-face-font 'variable-pitch "Bitstream Vera Sans Mono-9")
-;複数フレームに対応
+; multiple frame
 (modify-all-frames-parameters (list (assq 'font (frame-parameters))))
-;日本語フォント
+; Japanese font
 (set-fontset-font (frame-parameter nil 'font)
                   'japanese-jisx0208
                   '("VL Gothic" . "unicode-bmp"))
-;変換のタイムラグなしに
+; anthy
 (setq anthy-accept-timeout 1)
-;YaTeX使うため
+; YaTeX
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/yatex/")
 
 ;; other fontset
@@ -47,29 +44,6 @@
     (set-face-font (car fontset-list) my-default-font)
     (setq fontset-list (cdr fontset-list))))
 (my-font-lock-face-reset)
-
-;; ;(setq my-default-font "VL Gothic")
-;; (setq my-default-font (frame-parameter nil 'font))
-;; ;(set-face-font ' my-default-font)
-;; (set-face-font 'variable-pitch my-default-font)
-;; (set-face-font 'fixed-pitch my-default-font)
-;; ;; font-lock
-;; (set-face-font 'font-lock-comment-face my-default-font)
-;; (set-face-font 'font-lock-keyword-face my-default-font)
-;; (set-face-font 'font-lock-function-name-face my-default-font)
-;; (set-face-font 'font-lock-doc-face my-default-font)
-;; (set-face-font 'font-lock-string-face my-default-font)
-;; (set-face-font 'font-lock-builtin-face my-default-font)
-;; ;; org-mode
-;; (set-face-font 'org-level-2 my-default-font)
-;; (set-face-font 'org-level-3 my-default-font)
-;; (set-face-font 'org-level-4 my-default-font)
-;; (set-face-font 'org-level-5 my-default-font)
-;; (set-face-font 'org-level-6 my-default-font)
-;; (set-face-font 'org-level-7 my-default-font)
-;; (set-face-font 'org-level-8 my-default-font)
-;; (set-face-font 'org-todo my-default-font)
-;; (set-face-font 'org-done my-default-font)
 
 ;; Return face used at point
 (defun describe-face-at-point ()
