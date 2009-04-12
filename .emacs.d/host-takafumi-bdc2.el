2ede194890abcbc9b80305df0d161d9ca78ea454
Takafumi ARAKAKI <takafumi@takafumi-bdc2.(none)>
2009-04-12 14:37:30 +0900

    screen copy to x

--- .emacs.d/host-takafumi-bdc2.el
+++ .emacs.d/host-takafumi-bdc2.el
@@ -2,3 +2,10 @@
 (if window-system
     (color-theme-taylor);if not 'emacs -nw'
   )
+
+(cond
+ ;; emacs 23
+ ((string-match "^23\." emacs-version)
+  (load-file-in-dir preferences-directory
+		    "host-takafumi-bdc2-ver-23.el"))
+ )
