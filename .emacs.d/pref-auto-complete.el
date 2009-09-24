612bd5111d61d73c398a9224321f66c0a8ae4cad
Takafumi Arakaki <takafumi.a@gmail.com>
2009-09-25 02:50:45 +0900

    auto-complete-mode with pysmell

---
+++ .emacs.d/pref-auto-complete.el
@@ -0,0 +1,3 @@
+; auto-complete-mode
+(require 'auto-complete)
+;(global-auto-complete-mode t)
---
+++ .emacs.d/pref-pysmell.el
@@ -0,0 +1,16 @@
+; pysmell
+(require 'pysmell)
+(add-hook 'python-mode-hook (lambda () (pysmell-mode 1)))
+
+; pysmell from auto-complete-mode
+(defvar ac-source-pysmell
+  '((candidates
+     . (lambda ()
+         (require 'pysmell)
+         (pysmell-get-all-completions))))
+  "Source for PySmell")
+
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (set (make-local-variable 'ac-sources) 
+		  (append ac-sources '(ac-source-pysmell)))))
