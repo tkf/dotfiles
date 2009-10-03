5386152a5cad3e36030a6449b63c32d1eae2aeef
takafumi <takafumi@takafumi-bdc1.(none)>
2009-10-04 07:20:27 +0900

    font size changing func. font size 12 for 2:1 = jp:ascii

--- .emacs.d/host-takafumi-bdc1-ver-23.el
+++ .emacs.d/host-takafumi-bdc1-ver-23.el
@@ -9,3 +9,36 @@
 (setq anthy-accept-timeout 1)
 ; YaTeX
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/yatex/")
+
+; set frame width and height with command
+(defun my-frame-2c-s12 ()
+  (interactive)
+  (set-default-font "VL Gothic-12")
+  (set-frame-width (selected-frame) 164)
+  (set-frame-height (selected-frame) 56)
+  )
+
+(defun my-frame-2c-s10 ()
+  (interactive)
+  (set-default-font "VL Gothic-10")
+  (set-frame-width (selected-frame) 166)
+  (set-frame-height (selected-frame) 70)
+  )
+
+(defun my-frame-1c-s12 ()
+  (interactive)
+  (set-default-font "VL Gothic-12")
+  (set-frame-width (selected-frame) 81)
+  (set-frame-height (selected-frame) 56)
+  )
+
+(defun my-frame-1c-s10 ()
+  (interactive)
+  (set-default-font "VL Gothic-10")
+  (set-frame-width (selected-frame) 81)
+  (set-frame-height (selected-frame) 70)
+  )
+
+(defun my-set-font-size (n)
+  (interactive "n")
+  (set-default-font (concat "VL Gothic-" (number-to-string n))))
